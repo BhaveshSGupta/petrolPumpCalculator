568ca472e6b1bc77333c0cb03f6e1c0977601e3d
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-06-29 13:35:14 +0530

    Added Base code

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const port = 3245
+const app = express()
+app.use(express.json())
+
+
+app.get('/',(request, response)=>{
+    response.send('<h1>Hello World</h1>')
+})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+// app.use(cors())
+// app.use(express.json())
