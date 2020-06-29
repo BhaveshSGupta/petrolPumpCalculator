24579c53bfe4da36d5f35370145bc5e5734d3875
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-06-29 15:10:56 +0530

    Updates in model

--- models/keshavPump.js
+++ models/keshavPump.js
@@ -64,27 +64,27 @@ const KeshavProtrolPump = new mongoose.Schema({
         type: String,
         trim:true,
       },
-      Testing_MS_Dispenser_1_Nozle_1:{
+      Testing_HSD_Dispenser_1_Nozle_1:{
         type: String,
         trim:true,
       },
-      Testing_MS_Dispenser_1_Nozle_2:{
+      Testing_HSD_Dispenser_1_Nozle_2:{
         type: String,
         trim:true,
       },
-      Testing_MS_Dispenser_2_Nozle_1:{
+      Testing_HSD_Dispenser_2_Nozle_1:{
         type: String,
         trim:true,
       },
-      Testing_MS_Dispenser_2_Nozle_2:{
+      Testing_HSD_Dispenser_2_Nozle_2:{
         type: String,
         trim:true,
       },
-      Testing_MS_Dispenser_3_Nozle_1:{
+      Testing_HSD_Dispenser_3_Nozle_1:{
         type: String,
         trim:true,
       },
-      Testing_MS_Dispenser_3_Nozle_2:{
+      Testing_HSD_Dispenser_3_Nozle_2:{
         type: String,
         trim:true,
       }
