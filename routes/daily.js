3e8bd4f74e7737e1f16a02eb31edcc1e58f5a286
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-06-29 21:56:39 +0530

    Add methods for calulate volume

--- routes/daily.js
+++ routes/daily.js
@@ -1,16 +1,38 @@
 const express = require('express')
 const router = new express.Router()
 const volumneStaticData = require('../excelConverter/index')
-
 const Dailydata = require('../models/dailyData')
 
+const calVolofPetrol = (number) => {
+  const intNumber = parseInt(number)
+  if(intNumber < number){
+    return (number - intNumber) * volumneStaticData.MS[intNumber].DIFFERENCE + volumneStaticData.MS[intNumber].VOLUME
+  } 
+  return volumneStaticData.MS[intNumber].VOLUME
+}
+
+const calVolofDiesel = (number) => {
+  const intNumber = parseInt(number)
+  if(intNumber < number){
+    return (number - intNumber) * volumneStaticData.HSD[intNumber].DIFFERENCE + volumneStaticData.HSD[intNumber].VOLUME
+  } 
+  return volumneStaticData.HSD[intNumber].VOLUME
+}
+
 router.post('/daily', async (request, response)=>{
-  // console.log(volumneStaticData)
   const inputData = {...request.body}
-    const dailydata = new Dailydata() 
+  // console.log(inputData)
+  const volumeofPetrolTank = calVolofPetrol(inputData.MS_DIP)
+  // console.log(volumeofPetrolTank)
+    const dailydata = new Dailydata({
+      ...request.body,
+      "Volume_in_MS":calVolofPetrol(inputData.MS_DIP),
+      "Volume_in_HSD_DIP1":calVolofDiesel(inputData.HSD_DIP1),
+      "Volume_in_HSD_DIP2":calVolofDiesel(inputData.HSD_DIP2)
+    }) 
     try {
         await dailydata.save()
-        response.status(201).send(task)
+        response.status(201).send(dailydata)
       } catch (e) {
         response.status(400).send(e)
       }
