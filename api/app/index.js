c7c38e426f97ee9d9f85444b9faff8353fc91a18
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-07-17 08:36:14 +0530

    add things to backend

--- api/app/index.js
+++ api/app/index.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const app = express()
+const session = require('express-session')
+const passport = require('passport')
 app.use(express.json())
-
+app.use(session({
+    secret:'thesecret',
+    saveUninitialized:false,
+    resave:false
+}))
 module.exports = app;
--- api/authentication/index.js
+++ api/authentication/index.js
@@ -1,22 +1,33 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+const passport = require('passport')
 
 const auth = async (req, res, next) => {
     try {
+        passport.use(new LocalStrategy(
+            function(username, password, done) {
+              User.findOne({ username: username }, function (err, user) {
+                if (err) { return done(err); }
+                if (!user) { return done(null, false); }
+                if (!user.verifyPassword(password)) { return done(null, false); }
+                return done(null, user);
+              });
+            }
+          ));
+          
         const token = req.header('Authorization').replace('Bearer', '').trim()
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findOne({_id:decoded._id, 'tokens.token': token})
-        
-        if(!user) {
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token }).lean()
+        if (!user) {
             throw new Error()
         }
-        req.token =  token
+        req.token = token
         req.user = user
-        
         next()
     } catch (e) {
-        res.status(401).send({ error: 'Please authenticate.'})
+        res.status(401).send({ error: 'Please authenticate.' })
     }
 }
+
 
 module.exports = auth
