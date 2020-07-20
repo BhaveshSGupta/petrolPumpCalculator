d2885c19bbf48fe471ff8481050ecfaf1740e997
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-07-20 09:27:49 +0530

    before adding yup

--- src/components/daily/index.jsx
+++ src/components/daily/index.jsx
@@ -1,77 +1,96 @@
-import React, { useState } from 'react';
-import useCustomForm from './hooks/'
-const initialValues = {
-    "MS": "",
-    "HSD": "",
-    "MS_DIP": "",
-    "HSD_DIP1": "",
-    "HSD_DIP2": "",
-    "Closing_Reading_MS_Dispenser_1_Nozle_1": "",
-    "Closing_Reading_MS_Dispenser_1_Nozle_2": "",
-    "Testing_MS_Dispenser_1_Nozle_1": "15",
-    "Testing_MS_Dispenser_1_Nozle_2": "15",
-    "Closing_Reading_HSD_Dispenser_1_Nozle_1": "",
-    "Closing_Reading_HSD_Dispenser_1_Nozle_2": "",
-    "Closing_Reading_HSD_Dispenser_2_Nozle_1": "",
-    "Closing_Reading_HSD_Dispenser_2_Nozle_2": "",
-    "Closing_Reading_HSD_Dispenser_3_Nozle_1": "",
-    "Closing_Reading_HSD_Dispenser_3_Nozle_2": "",
-    "Testing_HSD_Dispenser_1_Nozle_1": "15",
-    "Testing_HSD_Dispenser_1_Nozle_2": "15",
-    "Testing_HSD_Dispenser_2_Nozle_1": "15",
-    "Testing_HSD_Dispenser_2_Nozle_2": "15",
-    "Testing_HSD_Dispenser_3_Nozle_1": "15",
-    "Testing_HSD_Dispenser_3_Nozle_2": "15"
+import React from "react";
+
+import { useFormik } from "formik";
+import './daily.css'
+// A custom validation function. This must return an object
+
+// which keys are symmetrical to our values/initialValues
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 15) {
+    errors.firstName = "Must be 15 characters or less";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length > 20) {
+    errors.lastName = "Must be 20 characters or less";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  return errors;
 };
+
 const Daily = () => {
-    const {
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit
-    } = useCustomForm({
-        initialValues,
-        onSubmit: values => console.log({ values })
-    });
-    return (
-        <form onSubmit={handleSubmit} className="App">
-        
+  // Pass the useFormik() hook initial form values and a submit function that will
 
-            <label>Name</label>
-            <input
-                type="text"
-                name="name"
-                onChange={handleChange}
-                value={values.name}
-                error={errors.name}
-            />
-            <br />
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
 
-            <label>Lastname</label>
-            <input
-                type="text"
-                name="lastName"
-                onChange={handleChange}
-                required
-                value={values.lastName}
-                onBlur={handleBlur}
-            />
-            <br />
+    validate,
 
-            <label>Age</label>
-            <input
-                type="number"
-                name="age"
-                onChange={handleChange}
-                value={values.age}
-                required
-            />
-            <br />
-            <button type="submit">Submit</button>
-        </form>
-    );
-}
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.firstName}
+      />
+
+      {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+
+      <label htmlFor="lastName">Last Name</label>
+
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastName}
+      />
+
+      {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+
+      <label htmlFor="email">Email Address</label>
+
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+
+      {formik.errors.lastName && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 export default Daily;
