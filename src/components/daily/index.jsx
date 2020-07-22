b99847bc1dc99adab268fec0b332926d0f1eff1e
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-07-22 09:39:31 +0530

    formating

--- src/components/daily/index.jsx
+++ src/components/daily/index.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-
 import { useFormik } from "formik";
 import "./daily.css";
 import * as Yup from "yup";
-// A custom validation function. This must return an object
-
-// which keys are symmetrical to our values/initialValues
 
 const Daily = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
-
   // be called when the form is submitted
   const formik = useFormik({
     initialValues: {
@@ -32,41 +27,24 @@ const Daily = () => {
   return (
     <form onSubmit={formik.handleSubmit}>
       <label htmlFor="firstName">First Name</label>
-
       <input
         id="firstName"
         type="text"
-        {...formik.getFieldProps('firstName')}
+        {...formik.getFieldProps("firstName")}
       />
-
       {formik.touched.firstName && formik.errors.firstName ? (
         <div>{formik.errors.firstName}</div>
       ) : null}
-
       <label htmlFor="lastName">Last Name</label>
-
-      <input
-        id="lastName"
-        type="text"
-        {...formik.getFieldProps('lastName')}
-      />
-
+      <input id="lastName" type="text" {...formik.getFieldProps("lastName")} />
       {formik.touched.lastName && formik.errors.lastName ? (
         <div>{formik.errors.lastName}</div>
       ) : null}
-
       <label htmlFor="email">Email Address</label>
-
-      <input
-        id="email"
-        type="email"
-        {...formik.getFieldProps('email')}
-      />
-
+      <input id="email" type="email" {...formik.getFieldProps("email")} />
       {formik.touched.email && formik.errors.email ? (
         <div>{formik.errors.email}</div>
       ) : null}
-
       <button type="submit">Submit</button>
     </form>
   );
