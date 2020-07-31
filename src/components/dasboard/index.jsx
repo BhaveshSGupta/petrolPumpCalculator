861d95ac4f95fced27eee36e1f0d5f34b86a85dd
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-01 04:54:30 +0530

    Initial work for dashboard

--- src/components/dasboard/index.jsx
+++ src/components/dasboard/index.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from "react"
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
+  useEffect(() => {
+    fetchData()
+  }, [])
+  const fetchData = async () => {
+    fetch("/api/dashboard?limit=5", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log({ response: res })
+      })
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
