e4d68c07558568b8c6ea35dd1313570dbd44506a
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-01 10:21:27 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import React from "react";
-// import Container from "@material-ui/core/Container";
-// import Typography from "@material-ui/core/Typography";
-// import Box from "@material-ui/core/Box";
-// import Link from "@material-ui/core/Link";
-import ProTip from "./components/ProTip";
-// import SignIn from './components/login/SignIn'
 import Daily from "./components/daily";
-// import Dashboard from "./components/dasboard";
-// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-// function Copyright() {
-//   return (
-//     <h6>Developed</h6>
-//   );
-// }
+import Home from "./components/home"
+import Dashboard from "./components/dashboard"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 
 export default function App() {
   return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/daily">Daily</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/daily">
+            <Daily />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
     <div>
-      <Daily />
-      <ProTip />
-      {/* <Copyright /> */}
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
     </div>
   );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
 }
