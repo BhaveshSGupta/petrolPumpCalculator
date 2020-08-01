fb7a8336723a380566e79217f27ab27d58d182c0
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-01 10:53:31 +0530

    Show date list

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react"
-
 const Dashboard = () => {
+  const [data, setData] = useState([])
   useEffect(() => {
     fetchData()
   }, [])
   const fetchData = async () => {
-    fetch("/api/dashboard?limit=5", {
+    fetch("/api/dashboard?limit=5&sortBy=date:desc", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -13,13 +13,18 @@ const Dashboard = () => {
     })
       .then(res => res.json())
       .then(res => {
-        console.log({ response: res })
+        setData(res)
       })
   }
 
   return (
     <div>
       <h1>Dashboard</h1>
+      {data.length > 0 &&
+        data.map((day, index) => {
+          let d = new Date(day.date)
+          return <div key={index}>{d.toLocaleString('en-IN')}</div>
+        })}
     </div>
   )
 }
