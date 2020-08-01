3e8bcecf763e7d695b470cb124f8be1359750967
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-01 10:52:56 +0530

    Need to understand how sort works rewatch video

--- api/dashboard/index.js
+++ api/dashboard/index.js
@@ -14,6 +14,7 @@ app.get('*', async (req, res) => {
         const parts = req.query.sortBy.split(':')
         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
+    console.log(sort)
     try {
         const data = await Dailydata.find(
             {}, null, {
