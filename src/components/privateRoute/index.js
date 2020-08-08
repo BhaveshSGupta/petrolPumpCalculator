dcc3bf1c82f75f1224e1929dea7f7440203765f0
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-08 17:45:12 +0530

    add auth

--- src/components/privateRoute/index.js
+++ src/components/privateRoute/index.js
@@ -2,21 +2,25 @@ import React from "react"
 import { Redirect, Route } from "react-router-dom"
 const fakeAuth = {}
 fakeAuth.isAuthenticated = false
+
 function PrivateRoute({ component: Component, ...rest }) {
+  const getUser = () => (JSON.parse(window.localStorage.loggedIn))
+  const auth = getUser()
+  // console.log({ auth })
   return (
     <Route
       {...rest}
       render={props =>
-        fakeAuth.isAuthenticated ? (
+        auth.isLoggedIn ? (
           <Component {...props} />
         ) : (
-          <Redirect
-            to={{
-              pathname: "/",
-              state: { from: props.location },
-            }}
-          />
-        )
+            <Redirect
+              to={{
+                pathname: "/",
+                state: { from: props.location },
+              }}
+            />
+          )
       }
     />
   )
