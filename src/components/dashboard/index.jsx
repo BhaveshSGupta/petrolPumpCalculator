3cd0ea1b2a2367abd310a9e30ae4fed89f5de94f
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-08 06:27:39 +0530

    Making it more beautiful

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -2,23 +2,7 @@ import React, { useState, useEffect } from "react"
 import useSWR from "swr"
 
 const Dashboard = () => {
-  // const [data, setData] = useState([])
   const fetcher = url => fetch(url).then(res => res.json())
-  // useEffect(() => {
-  //   fetchData()
-  // }, [])S
-  // const fetchData = async () => {
-  //   fetch("/api/dashboard?limit=5&sortBy=date:desc", {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   })
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       setData(res)
-  //     })
-  // }p
   const { data = [], error } = useSWR(
     "/api/dashboard?limit=5&sortBy=date:desc",
     fetcher
