6d863172897967e1ab5fb53d6bd65db9cfc75046
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-09 09:41:24 +0530

    fix in login to redirect to dashboard

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,9 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
 import { Formik, Field, Form, ErrorMessage } from "formik"
-import HandleLogin from "../../services/handlelogin"
-const Login = () => {
+import { Redirect } from "react-router-dom"
+// import HandleLogin from "../../services/handlelogin"
+const Login = props => {
+  const [redirect, setredirect] = useState(false)
+  const ApiUrl = "/api/users"
+  const setUser = user =>
+    window.localStorage.setItem("loggedIn", JSON.stringify(user))
+  const HandleLogin = async ({ email, password }) => {
+    // const history = useHistory();
+    try {
+      const requestOptions = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      }
+      fetch(ApiUrl, requestOptions)
+        .then(async response => {
+          if (response.status === 200) {
+            window.localStorage.setItem(
+              "loggedIn",
+              JSON.stringify({
+                isLoggedIn: true,
+              })
+            )
+            return { body: await response.json(), status: response.status }
+          } else {
+            return { body: "hello", status: response.status }
+          }
+        })
+        // .then(({ body, status }) => {
+        //   if (body.token && status === 200) {
+
+        //   } else {
+
+        //   }
+        // })
+        .then(() => {
+          setredirect(true)
+        })
+    } catch (e) {
+      // console.log(e)
+    }
+  }
   return (
     <div>
+      {redirect && (
+        <Redirect
+          to={{
+            pathname: "/dashboard",
+            state: { from: props.location },
+          }}
+        />
+      )}
       <Formik
         initialValues={{
           password: "",
