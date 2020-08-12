2d4044e62f91f6f3b92d28f0d618fbb7fe3c6f17
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-12 08:31:29 +0530

    Clearing and making api more compaliant

--- api/app/index.js
+++ api/app/index.js
@@ -1,11 +1,4 @@
 const express = require("express")
 const app = express()
-const session = require('express-session')
-const passport = require('passport')
 app.use(express.json())
-app.use(session({
-    secret:'thesecret',
-    saveUninitialized:false,
-    resave:false
-}))
 module.exports = app;
--- api/authentication/index.js
+++ api/authentication/index.js
@@ -9,19 +9,16 @@ const auth = async (req, res, next) => {
   connect('auth')
   try {
     const token = req.header('Authorization').replace('Bearer', '').trim()
-    // console.log({ token })
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token }).lean()
     if (!user) {
       throw new Error()
     }
     req.token = token
     req.user = user
-    // console.log({ decoded, token, user })
-    // // disconnect('auth')
     next()
   } catch (e) {
+    res.clearCookie("accessToken");
     res.status(401).send({ error: 'Please authenticate.' })
     disconnect('auth')
   }
