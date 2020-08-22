00a36528259eecd701332f8089d665fc8ec89dcd
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2020-08-22 08:11:26 +0530

    Better Error Reporting

--- api/app/index.js
+++ api/app/index.js
@@ -1,4 +1,17 @@
 const express = require("express")
 const app = express()
-app.use(express.json())
+// app.use(express.json())
+const expressJsonVerifier = function (request, response, next) {
+    express.json()(request, response, error => {
+        if (error) {
+            console.error(error);
+            console.log('Request recived with improper data')
+            return response.status(400).send({
+                'request': 'failed'
+            })
+        }
+        next();
+    })
+}
+app.use(expressJsonVerifier)
 module.exports = app
